68bebf4b70ebd04b4bdda7bb294d2f076590e8a7
OliverSaarela <43965623+OliverSaarela@users.noreply.github.com>
2021-02-24 18:00:01 +0200

    Cleaning comments

--- clean_team_data.py
+++ clean_team_data.py
@@ -8,16 +8,12 @@ from datetime import date
 
 def main():
 
-    #os.system("cd plstats")
-    #os.system("scrapy crawl alltime")
-    #os.system("cd ..")
-
     data = pd.read_json(r'plstats/alltime.json')
 
-    ## Having stat names in a list while manually adding 'Team'
-    ## and automatically adding every other name of data
-    ## E.G. Matches_played, Wins, Losses, ...
-    ## Formating all text to be readable
+    # Having stat names in a list while manually adding 'Team'
+    # and automatically adding every other name of data
+    # E.G. Matches_played, Wins, Losses, ...
+    # Formating all text to be readable
     data_names = ['Team']
     for i in data['names'][0]:
         names = re.findall(r'\w+', i)
@@ -25,10 +21,10 @@ def main():
         if len(names) > 0 and names not in ['Attack', 'Team_Play', 'Defence', 'Discipline']:# Manually removing stat categories
             data_names.append(names)
 
-    ## Taking all the data that matches the stat names
-    ## and combining each teams list of data
-    ## to a list (Lists inside a list)
-    ## Also formating all text to be readable
+    # Taking all the data that matches the stat names
+    # and combining each teams list of data
+    # to a list (Lists inside a list)
+    # Also formating all text to be readable
     data_list = []
     for team in range(len(data)):
         team_data = []
@@ -44,9 +40,9 @@ def main():
             team_data.append(value)
         data_list.append(team_data)
 
-    ## Truning the data into a pandas dataframe and filling empty slots
-    ## also removing duplicated columns
-    ## and saving it as a csv
+    # Truning the data into a pandas dataframe and filling empty slots
+    # also removing duplicated columns
+    # and saving it as a csv
     alldata_df = pd.DataFrame(np.array(data_list), columns=data_names)
     alldata_df.replace(to_replace='', value=0, inplace=True)
     alldata_df = alldata_df.loc[:,~alldata_df.columns.duplicated()]
